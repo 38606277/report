815510fb060914fbe7778bc3a9e7b72bef1c0bc4
808455555 <zhangjinxiang8@126.com>
2018-04-26 12:05:52 +0800

    exclude datasource

--- src/main/java/root/ReportServerApplication.java
+++ src/main/java/root/ReportServerApplication.java
@@ -3,10 +3,11 @@ package root;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication(exclude={MongoAutoConfiguration.class,MongoDataAutoConfiguration.class})
+@SpringBootApplication(exclude={MongoAutoConfiguration.class,MongoDataAutoConfiguration.class,DataSourceAutoConfiguration.class})
 @PropertySource(value={"file:config/application.properties"})
 public class ReportServerApplication {
 
