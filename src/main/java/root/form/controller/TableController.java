40faa83a3a2c471f5bfdac2bf471e18e254c86ab
38606277 <38606277@qq.com>
2018-04-26 23:38:21 +0800

    form设计

--- src/main/java/root/form/controller/TableController.java
+++ src/main/java/root/form/controller/TableController.java
@@ -1,10 +1,45 @@
 package root.form.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import root.report.common.RO;
 
 @RestController
-public class TableController {
+public class TableController  extends RO {
 
+
+    @RequestMapping(value = "/CreateTable")
+    public String CreateTable(){
+
+
+        return  SuccessMsg("","");
+
+    }
+
+
+
+    @RequestMapping(value = "/getData/{templateID}")
+    public String GetData(String templateID){
+
+
+        return  SuccessMsg("","");
+
+    }
+    @RequestMapping(value = "/getData/{templateID}/{taskID}")
+    public String GetData(String templateID,String taskID){
+
+
+        return  SuccessMsg("","");
+
+    }
+
+    @RequestMapping(value = "/getData/{templateID}/{taskID}/{userID}")
+    public String GetData(String templateID,String taskID,String userID){
+
+
+        return  SuccessMsg("","");
+
+    }
 
 
 }
