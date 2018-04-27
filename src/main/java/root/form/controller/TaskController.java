6af37911aadfafea6922fcb8b60a9a946a425c44
38606277 <38606277@qq.com>
2018-04-27 08:26:40 +0800

    form设计

--- src/main/java/root/form/controller/TaskController.java
+++ src/main/java/root/form/controller/TaskController.java
@@ -12,6 +12,12 @@ public class TaskController extends RO {
     @RequestMapping(value = "/getTask/{templateID}")
     public  String CreateTask(){
 
+        //写任务表
+
+        //写任务用户表，处理事务
+
+
+
         return  SuccessMsg("","");
     }
 
@@ -29,6 +35,16 @@ public class TaskController extends RO {
         return  SuccessMsg("","");
     }
 
+
+    //修改模板任务
+    @RequestMapping(value = "/UpdateTaskState/{templateID}")
+    public  String UpdateTaskState(){
+
+        return  SuccessMsg("","");
+    }
+
+
+
     //获取当前模板下的任务
     @RequestMapping(value = "/getTask/{templateID}")
     public  String GetTask(){
@@ -38,5 +54,18 @@ public class TaskController extends RO {
 
 
 
+    //获取待办任务
+    @RequestMapping(value = "/getTask/{UserID}")
+    public  String GetUnTask(){
+
+        return  SuccessMsg("","");
+    }
+
+    //提交任务
+    @RequestMapping(value = "/getTask/{UserID}")
+    public  String CommitTask(){
+
+        return  SuccessMsg("","");
+    }
 
 }
