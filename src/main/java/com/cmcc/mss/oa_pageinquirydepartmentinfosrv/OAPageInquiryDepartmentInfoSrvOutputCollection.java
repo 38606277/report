ca384d07a13fe69a6a6fefeca28a7912197047a0
808455555 <zhangjinxiang8@126.com>
2018-05-29 14:26:39 +0800

    login url

--- src/main/java/com/cmcc/mss/oa_pageinquirydepartmentinfosrv/OAPageInquiryDepartmentInfoSrvOutputCollection.java
+++ src/main/java/com/cmcc/mss/oa_pageinquirydepartmentinfosrv/OAPageInquiryDepartmentInfoSrvOutputCollection.java
@@ -8,26 +8,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
-
-/**
- * <p>OA_PageInquiryDepartmentInfoSrvOutputCollection complex type的 Java 类。
- * 
- * <p>以下模式片段指定包含在此类中的预期内容。
- * 
- * <pre>
- * &lt;complexType name="OA_PageInquiryDepartmentInfoSrvOutputCollection"&gt;
- *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;sequence&gt;
- *         &lt;element name="OA_PageInquiryDepartmentInfoSrvOutputItem" type="{http://mss.cmcc.com/OA_PageInquiryDepartmentInfoSrv}OA_PageInquiryDepartmentInfoSrvOutputItem" maxOccurs="unbounded" minOccurs="0"/&gt;
- *       &lt;/sequence&gt;
- *     &lt;/restriction&gt;
- *   &lt;/complexContent&gt;
- * &lt;/complexType&gt;
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "OA_PageInquiryDepartmentInfoSrvOutputCollection", propOrder = {
     "oaPageInquiryDepartmentInfoSrvOutputItem"
