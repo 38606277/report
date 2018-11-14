3820749ea971ecbfb9f7b8f6eb41d920cc5e75ad
admin <123456>
2018-11-14 18:40:56 +0800

    [新增]:增加二级缓存功能

--- src/main/java/root/report/util/ExecuteSqlUtil.java
+++ src/main/java/root/report/util/ExecuteSqlUtil.java
@@ -1,13 +1,27 @@
 package root.report.util;
 
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.cache.TransactionalCacheManager;
+import org.apache.ibatis.cache.decorators.TransactionalCache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.apache.ibatis.executor.CachingExecutor;
 import org.apache.ibatis.mapping.*;
 import org.apache.ibatis.scripting.LanguageDriver;
 import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.transaction.Transaction;
 import org.apache.log4j.Logger;
 import org.apache.poi.ss.formula.functions.T;
+import org.mybatis.caches.ehcache.LoggingEhcache;
+import root.report.db.DbFactory;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +34,8 @@ import java.util.Map;
 public class ExecuteSqlUtil {
 
     private static Logger log = Logger.getLogger(ExecuteSqlUtil.class);
+
+    private final static TransactionalCacheManager tcm = new TransactionalCacheManager();
 
     /**
      *
@@ -34,23 +50,77 @@ public class ExecuteSqlUtil {
         if(statementType==null){
             statementType = StatementType.PREPARED; // 默认为 prepared
         }
+
+        List<?> list = null;
+        List<?> cacheList = null;
+        CacheKey cacheKey = null;
+        if(bounds==null){
+            bounds = new RowBounds();
+        }
+
         // 1. 对executeSql 加上script标签
         StringBuffer sb = new StringBuffer();
         sb.append("<script>");
         sb.append(executeSql);
         sb.append("</script>");
-        log.info(sb.toString());
+        log.info("转换后的sql为:->"+sb.toString());
         Configuration configuration = sqlSession.getConfiguration();
+        configuration.setCacheEnabled(true);  // 开启二级缓存?
+
         LanguageDriver languageDriver = configuration.getDefaultScriptingLanguageInstance();  // 2. languageDriver 是帮助我们实现dynamicSQL的关键
         SqlSource sqlSource = languageDriver.createSqlSource(configuration,sb.toString(),clazz);  //  泛型化入参
-        newSelectMappedStatement(configuration,namespace+"."+mapper_id,sqlSource,clazz,statementType);
-        List<?> list = null;
-        if(bounds!=null){
-            list = sqlSession.selectList(namespace+"."+mapper_id,param,bounds);
+      //  configuration.getCaches().forEach(e -> System.out.print(e.getId()));
+        MappedStatement ms = null;
+        // 如果我们从 configuration 当中可以取得到的话，则看缓存当中是否存在
+        if(configuration.getMappedStatementNames().contains(namespace+"."+mapper_id)){
+            ms = configuration.getMappedStatement(namespace+"."+mapper_id);
         }else {
-            list = sqlSession.selectList(namespace+"."+mapper_id,param);
+            log.info("======不存在此mappedStatment=====");
         }
+        if(ms == null){
+            ms =  newSelectMappedStatement(configuration,namespace+"."+mapper_id,sqlSource,clazz,statementType);
+        }
+        // 组装cache
+        cacheKey = sqlSession.getConfiguration().newExecutor(new Transaction() {
+            @Override
+            public Connection getConnection() throws SQLException {
+                return this.getConnection();
+            }
+            @Override
+            public void commit() throws SQLException {
+                this.getConnection().commit();
+            }
+            @Override
+            public void rollback() throws SQLException {
+                this.getConnection().rollback();
+            }
+            @Override
+            public void close() throws SQLException {
+                this.getConnection().close();
+            }
+            @Override
+            public Integer getTimeout() throws SQLException {
+                return 5000;
+            }
+        }, ExecutorType.SIMPLE).createCacheKey(ms,param,bounds,ms.getBoundSql(param));
+        cacheList = (List<?>)tcm.getObject(ms.getCache(),cacheKey);
 
+        if(cacheList!=null && cacheList.size()>0){
+            log.info("cache hit  缓存命中,命中率为:");
+            return cacheList;
+        }else {
+            if(bounds!=null){
+                list = sqlSession.selectList(namespace+"."+mapper_id,param,bounds);
+                log.info("执行了一次查询");
+            }else {
+                list = sqlSession.selectList(namespace+"."+mapper_id,param);
+                log.info("执行了一次查询");
+            }
+            // 装入缓存
+            if(cacheKey!=null){
+                tcm.putObject(ms.getCache(),cacheKey,list);
+            }
+        }
         return list;
     }
 
@@ -73,6 +143,8 @@ public class ExecuteSqlUtil {
         MappedStatement ms = new MappedStatement.Builder(
                 configuration, msId, sqlSource, SqlCommandType.SELECT)
                 .statementType(statementType)
+                .cache(new LoggingEhcache(msId))
+                .useCache(true)
                 .resultMaps(new ArrayList<ResultMap>() {
                     {
                         add(new ResultMap.Builder(configuration,
@@ -87,5 +159,7 @@ public class ExecuteSqlUtil {
         }
         return ms;
     }
+
+
 
 }
