c944a84c588b4e255177eabc1de85ab1c4759bd2
38606277 <38606277@qq.com>
2018-12-05 22:47:16 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	work/dbtemplate/dictionary/数据字典.xml

---
+++ app/angular/vue/b.vue
@@ -0,0 +1,6 @@
+		<template id="about">
+			<div>
+				<h1>About</h1>
+				<p>This is the tutorial about vue-router.</p>
+			</div>
+		</template>
--- app/webapp/vue/a.vue
+++
@@ -1,6 +0,0 @@
-<template id="home">
-			<div>
-				<h1>Home</h1>
-				<p>{{msg}}</p>
-			</div>
-	</template>
