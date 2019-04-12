2a4147d9ccc44f6985fab3a76029fbc8267c8aa1
gaoluo <luo.gao@pccw.com>
2019-04-12 13:51:07 +0800

    语音合成

--- src/main/java/root/myVoice/MyVoiceApplication.java
+++ src/main/java/root/myVoice/MyVoiceApplication.java
@@ -1,17 +1,27 @@
 package root.myVoice;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.iflytek.cloud.speech.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import root.report.common.RO;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+
+
+
 
 @SpringBootApplication
 @RestController
@@ -58,6 +68,88 @@ public class MyVoiceApplication extends RO {
 
 		return SuccessMsg("语音识别", map);
 	}
+
+
+
+
+	@RequestMapping(value = "/yuyin", produces = "text/plain;charset=UTF-8")
+	public String  yuyin(@RequestBody String pJson, HttpServletResponse response) throws Exception , IOException {
+		JSONObject json = JSON.parseObject(pJson);
+		String data=json.getString("data");
+
+			System.out.println(data);
+			//换成你在讯飞申请的APPID
+			SpeechUtility.createUtility("appid=5cac4812");
+
+			//合成监听器
+			SynthesizeToUriListener synthesizeToUriListener = XunfeiLib.getSynthesize();
+
+			String fileName=XunfeiLib.getFileName("tts_test.pcm");
+			XunfeiLib.delDone(fileName);
+
+			//1.创建SpeechSynthesizer对象
+			SpeechSynthesizer mTts= SpeechSynthesizer.createSynthesizer( );
+			//2.合成参数设置，详见《MSC Reference Manual》SpeechSynthesizer 类
+			mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan");//设置发音人
+			mTts.setParameter(SpeechConstant.SPEED, "50");//设置语速，范围0~100
+			mTts.setParameter(SpeechConstant.PITCH, "50");//设置语调，范围0~100
+			mTts.setParameter(SpeechConstant.VOLUME, "50");//设置音量，范围0~100
+
+			//3.开始合成
+			//设置合成音频保存位置（可自定义保存位置），默认保存在“./tts_test.pcm”
+			mTts.synthesizeToUri(data,fileName ,synthesizeToUriListener);
+
+			//设置最长时间
+			int timeOut=30;
+			int star=0;
+
+			//校验文件是否生成
+			while(!XunfeiLib.checkDone(fileName)){
+
+				try {
+					Thread.sleep(1000);
+					star++;
+					if(star>timeOut){
+						throw new Exception("合成超过"+timeOut+"秒！");
+					}
+				} catch (Exception e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+					break;
+				}
+
+			}
+		Map<String,Object> map = new HashMap<String,Object>();
+		//输出 wav IO流
+		try{
+			response.setHeader("Content-Type", "audio/mpeg");
+			File file = new File(fileName);
+			int len_l = (int) file.length();
+			byte[] buf = new byte[2048];
+			FileInputStream fis = new FileInputStream(file);
+			OutputStream out = response.getOutputStream();
+
+			//写入WAV文件头信息
+			out.write(XunfeiLib.getWAVHeader(len_l,8000,2,16));
+
+			len_l = fis.read(buf);
+			while (len_l != -1) {
+				out.write(buf, 0, len_l);
+				len_l = fis.read(buf);
+			}
+			out.flush();
+			out.close();
+			fis.close();
+			map.put("wavSourceIO",out);
+			//删除文件和清除队列信息
+			XunfeiLib.delDone(fileName);
+			file.delete();
+		}catch (Exception e){
+			System.out.println(e);
+		}
+		return SuccessMsg("",map);
+	}
+
 
 	private void uploadUserWords(String aa) throws Exception {
 		SpeechUtility.createUtility("appid=5cac4812");//申请的appid
