ad250e3b1e19f8f51d172c3fe764ab23a2368478
gaoluo <luo.gao@pccw.com>
2019-04-15 12:53:41 +0800

    语音合成

--- src/main/java/root/myVoice/MyVoiceApplication.java
+++ src/main/java/root/myVoice/MyVoiceApplication.java
@@ -1,17 +1,14 @@
 package root.myVoice;
 
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.iflytek.cloud.speech.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import root.report.common.RO;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
@@ -55,11 +52,11 @@ public class MyVoiceApplication extends RO {
 		}
 		System.out.println("讯飞识别的语音结果："+words);
 
-		if(null==words && "".equals(words)){
+		if(null==words || "".equals(words)){
 			System.out.println("讯飞识别的语音结果：null");
 			map.put("status","error");
 			map.put("content","对不起，请您在描述一遍！");
-			return ErrorMsg("语音识别","没有获取到语音信息");
+			return ErrorMsg("语音识别失败","没有获取到语音信息");
 		}
 		String result = SR2Words.sr2words(words);
 		System.out.println("讯飞识别的语音结果："+result);
@@ -69,24 +66,18 @@ public class MyVoiceApplication extends RO {
 		return SuccessMsg("语音识别", map);
 	}
 
-
-
-
-	@RequestMapping(value = "/yuyin", produces = "text/plain;charset=UTF-8")
-	public String  yuyin(@RequestBody String pJson, HttpServletResponse response) throws Exception , IOException {
-		JSONObject json = JSON.parseObject(pJson);
-		String data=json.getString("data");
-
+	@RequestMapping(value="/yuyin")
+	public void getDataAuthList(@RequestBody String data, HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+			request.setCharacterEncoding("UTF-8");//解决乱码
+			//String data= URLDecoder.decode(request.getParameter("data"),"UTF-8");
 			System.out.println(data);
 			//换成你在讯飞申请的APPID
 			SpeechUtility.createUtility("appid=5cac4812");
-
 			//合成监听器
 			SynthesizeToUriListener synthesizeToUriListener = XunfeiLib.getSynthesize();
-
 			String fileName=XunfeiLib.getFileName("tts_test.pcm");
 			XunfeiLib.delDone(fileName);
-
 			//1.创建SpeechSynthesizer对象
 			SpeechSynthesizer mTts= SpeechSynthesizer.createSynthesizer( );
 			//2.合成参数设置，详见《MSC Reference Manual》SpeechSynthesizer 类
@@ -94,18 +85,14 @@ public class MyVoiceApplication extends RO {
 			mTts.setParameter(SpeechConstant.SPEED, "50");//设置语速，范围0~100
 			mTts.setParameter(SpeechConstant.PITCH, "50");//设置语调，范围0~100
 			mTts.setParameter(SpeechConstant.VOLUME, "50");//设置音量，范围0~100
-
 			//3.开始合成
 			//设置合成音频保存位置（可自定义保存位置），默认保存在“./tts_test.pcm”
 			mTts.synthesizeToUri(data,fileName ,synthesizeToUriListener);
-
 			//设置最长时间
-			int timeOut=30;
+			int timeOut=60;
 			int star=0;
-
 			//校验文件是否生成
 			while(!XunfeiLib.checkDone(fileName)){
-
 				try {
 					Thread.sleep(1000);
 					star++;
@@ -117,37 +104,31 @@ public class MyVoiceApplication extends RO {
 					e.printStackTrace();
 					break;
 				}
-
 			}
-		Map<String,Object> map = new HashMap<String,Object>();
-		//输出 wav IO流
-		try{
-			response.setHeader("Content-Type", "audio/mpeg");
-			File file = new File(fileName);
-			int len_l = (int) file.length();
-			byte[] buf = new byte[2048];
-			FileInputStream fis = new FileInputStream(file);
-			OutputStream out = response.getOutputStream();
-
-			//写入WAV文件头信息
-			out.write(XunfeiLib.getWAVHeader(len_l,8000,2,16));
-
-			len_l = fis.read(buf);
-			while (len_l != -1) {
-				out.write(buf, 0, len_l);
+			//输出 wav IO流
+			try{
+				response.setHeader("Content-Type", "audio/mpeg");
+				File file = new File(fileName);
+				int len_l = (int) file.length();
+				byte[] buf = new byte[2048];
+				FileInputStream fis = new FileInputStream(file);
+				OutputStream out = response.getOutputStream();
+				//写入WAV文件头信息
+				out.write(XunfeiLib.getWAVHeader(len_l,8000,2,16));
 				len_l = fis.read(buf);
+				while (len_l != -1) {
+					out.write(buf, 0, len_l);
+					len_l = fis.read(buf);
+				}
+				out.flush();
+				out.close();
+				fis.close();
+				//删除文件和清除队列信息
+				XunfeiLib.delDone(fileName);
+				file.delete();
+			}catch (Exception e){
+				System.out.println(e);
 			}
-			out.flush();
-			out.close();
-			fis.close();
-			map.put("wavSourceIO",out);
-			//删除文件和清除队列信息
-			XunfeiLib.delDone(fileName);
-			file.delete();
-		}catch (Exception e){
-			System.out.println(e);
-		}
-		return SuccessMsg("",map);
 	}
 
 
