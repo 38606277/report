b7e1338274ddfcc8c661d24929cb12d89b47a5c5
gaoluo <luo.gao@pccw.com>
2019-04-23 16:10:16 +0800

    问答在新建的时候会自动生成语音blob字节流，数据库字段类型是longBlob

--- src/main/java/root/report/service/QuestionService.java
+++ src/main/java/root/report/service/QuestionService.java
@@ -1,10 +1,16 @@
 package root.report.service;
 
 import com.alibaba.fastjson.JSONObject;
+import com.iflytek.cloud.speech.SpeechConstant;
+import com.iflytek.cloud.speech.SpeechSynthesizer;
+import com.iflytek.cloud.speech.SpeechUtility;
+import com.iflytek.cloud.speech.SynthesizeToUriListener;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+import root.myVoice.XunfeiLib;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,9 +22,72 @@ public class QuestionService {
     /**
      * 功能描述: 根据JSON数据解析 对应数据，生成问题库记录
      */
-    public String createQuestion(SqlSession sqlSession,JSONObject jsonObject){
+    public String createQuestion(SqlSession sqlSession,JSONObject jsonObject) throws IOException {
         Map<String,Object> map  = new HashMap<>();
         map.put("ai_question",jsonObject.getString("ai_question"));
+        //换成你在讯飞申请的APPID
+        SpeechUtility.createUtility("appid=5cac4812");
+        //合成监听器
+        SynthesizeToUriListener synthesizeToUriListener = XunfeiLib.getSynthesize();
+        String fileName=XunfeiLib.getFileName("ttstestq.pcm");
+        XunfeiLib.delDone(fileName);
+        //1.创建SpeechSynthesizer对象
+        SpeechSynthesizer mTts= SpeechSynthesizer.createSynthesizer( );
+        //2.合成参数设置，详见《MSC Reference Manual》SpeechSynthesizer 类
+        mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan");//设置发音人
+        mTts.setParameter(SpeechConstant.SPEED, "50");//设置语速，范围0~100
+        mTts.setParameter(SpeechConstant.PITCH, "50");//设置语调，范围0~100
+        mTts.setParameter(SpeechConstant.VOLUME, "50");//设置音量，范围0~100
+        //3.开始合成
+        //设置合成音频保存位置（可自定义保存位置），默认保存在“./tts_test.pcm”
+        mTts.synthesizeToUri(jsonObject.getString("ai_question"),fileName ,synthesizeToUriListener);
+        //设置最长时间
+        int timeOut=60;
+        int star=0;
+        //校验文件是否生成
+        while(!XunfeiLib.checkDone(fileName)){
+            try {
+                Thread.sleep(1000);
+                star++;
+                if(star>timeOut){
+                    throw new Exception("合成超过"+timeOut+"秒！");
+                }
+            } catch (Exception e) {
+                // TODO 自动生成的 catch 块
+                e.printStackTrace();
+                break;
+            }
+        }
+        //输出 wav IO流
+            File file = new File(fileName);
+            int len_l = (int) file.length();
+            ByteArrayOutputStream out = null;
+            try {
+                FileInputStream in = new FileInputStream(file);
+                out = new ByteArrayOutputStream();
+                //写入WAV文件头信息
+                out.write(XunfeiLib.getWAVHeader(len_l,8000,2,16));
+                byte[] b = new byte[1024];
+                int i = 0;
+                while ((i = in.read(b)) != -1) {
+
+                    out.write(b, 0, b.length);
+                }
+                out.close();
+                in.close();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            byte[] buf = out.toByteArray();
+            //删除文件和清除队列信息
+            XunfeiLib.delDone(fileName);
+            file.delete();
+        map.put("fileDataBlob",buf);
+
         sqlSession.insert("question.createQuestion",map);
         return String.valueOf(map.get("id"));
     }
@@ -61,6 +130,68 @@ public class QuestionService {
         map.put("current",jsonObject.getString("current"));
         map.put("answer",jsonObject.getString("answer"));
         map.put("creat_by",jsonObject.getString("creat_by"));
+        //换成你在讯飞申请的APPID
+        SpeechUtility.createUtility("appid=5cac4812");
+        //合成监听器
+        SynthesizeToUriListener synthesizeToUriListener = XunfeiLib.getSynthesize();
+        String fileName=XunfeiLib.getFileName("ttstesta.pcm");
+        XunfeiLib.delDone(fileName);
+        //1.创建SpeechSynthesizer对象
+        SpeechSynthesizer mTts= SpeechSynthesizer.createSynthesizer( );
+        //2.合成参数设置，详见《MSC Reference Manual》SpeechSynthesizer 类
+        mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan");//设置发音人
+        mTts.setParameter(SpeechConstant.SPEED, "50");//设置语速，范围0~100
+        mTts.setParameter(SpeechConstant.PITCH, "50");//设置语调，范围0~100
+        mTts.setParameter(SpeechConstant.VOLUME, "50");//设置音量，范围0~100
+        //3.开始合成
+        //设置合成音频保存位置（可自定义保存位置），默认保存在“./tts_test.pcm”
+        mTts.synthesizeToUri(jsonObject.getString("answer"),fileName ,synthesizeToUriListener);
+        //设置最长时间
+        int timeOut=60;
+        int star=0;
+        //校验文件是否生成
+        while(!XunfeiLib.checkDone(fileName)){
+            try {
+                Thread.sleep(1000);
+                star++;
+                if(star>timeOut){
+                    throw new Exception("合成超过"+timeOut+"秒！");
+                }
+            } catch (Exception e) {
+                // TODO 自动生成的 catch 块
+                e.printStackTrace();
+                break;
+            }
+        }
+        //输出 wav IO流
+        File file = new File(fileName);
+        int len_l = (int) file.length();
+        ByteArrayOutputStream out = null;
+        try {
+            FileInputStream in = new FileInputStream(file);
+            out = new ByteArrayOutputStream();
+            //写入WAV文件头信息
+            out.write(XunfeiLib.getWAVHeader(len_l,8000,2,16));
+            byte[] b = new byte[1024];
+            int i = 0;
+            while ((i = in.read(b)) != -1) {
+
+                out.write(b, 0, b.length);
+            }
+            out.close();
+            in.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        byte[] buf = out.toByteArray();
+        //删除文件和清除队列信息
+        XunfeiLib.delDone(fileName);
+        file.delete();
+        map.put("fileDataBlob",buf);
         sqlSession.insert("question.createAnswer",map);
         return String.valueOf(map.get("id"));
     }
