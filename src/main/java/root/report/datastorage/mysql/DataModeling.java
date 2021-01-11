78790c46460f54dc51a61ed40d8d6e0cec58c94d
lijianzhong <Cueb0909>
2021-01-11 11:30:14 +0800

    hbase 通过phoenix创建表，注：表名，列名均大写

--- src/main/java/root/report/datastorage/mysql/DataModeling.java
+++ src/main/java/root/report/datastorage/mysql/DataModeling.java
@@ -277,28 +277,72 @@ public class DataModeling extends RO
     }
 
 
-    @RequestMapping(value="/createHiveTable",produces = "text/plain;charset=UTF-8")
-    public String createHiveTable(@RequestBody JSONObject pJson)  {
-        String tableName = pJson.getString("tableName");
-        Map<String,String> map = new HashMap<String,String>();
-        map.put("tableName", tableName);
-        DbFactory.Open("hive").selectList("datamodeling.createHiveTable",map);
-        return "";
-    }
+//    @RequestMapping(value="/createHiveTable",produces = "text/plain;charset=UTF-8")
+//    public String createHiveTable(@RequestBody JSONObject pJson)  {
+//        String tableName = pJson.getString("tableName");
+//        Map<String,String> map = new HashMap<String,String>();
+//        map.put("tableName", tableName);
+//        DbFactory.Open("hive").selectList("datamodeling.createHiveTable",map);
+//        return "";
+//    }
 
     @RequestMapping(value="/createHiveTable",produces = "text/plain;charset=UTF-8")
     public String createHiveTable2(@RequestBody JSONObject pJson)  {
         String tableName = pJson.getString("tableName");
         String tableFields = pJson.getString("tableFields");
-
         JSONArray tableFieldsArray=JSONArray.parseArray(tableFields);
-
         long tableId =IDUtil.getId();
         long modelId =IDUtil.getId();
-
         //构建表语句
         StringBuffer tableSql=new StringBuffer();
         tableSql.append("CREATE external TABLE IF NOT EXISTS  ");
+        tableSql.append("  ");
+        tableSql.append(tableName);
+        tableSql.append("  (");
+        for(int i=0;i<tableFieldsArray.size();i++){
+            JSONObject obj= (JSONObject) tableFieldsArray.get(i);
+            String field=obj.get("fieldName").toString();
+            String fieldtype=obj.get("fieldType").toString();
+            tableSql.append(field);
+            tableSql.append(" ");
+            tableSql.append(fieldtype);
+            tableSql.append(",");
+        }
+        tableSql.deleteCharAt(tableSql.length()-1);
+        tableSql.append("  )");
+        tableSql.append(" ROW FORMAT   ");
+        tableSql.append("  DELIMITED FIELDS TERMINATED BY ','");
+        tableSql.append("  STORED AS TEXTFILE ");
+        String aa =  tableSql.toString();
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("tableSql", tableSql.toString());
+        DbFactory.Open("hive").selectList("datamodeling.createHiveTable2",map);
+        DbFactory.close("hive");
+        return  tableSql.toString();
+    }
+
+
+//    @RequestMapping(value="/createHbaseTable",produces = "text/plain;charset=UTF-8")
+//    public String createHbaseTable(@RequestBody JSONObject pJson)  {
+//        String tableName = pJson.getString("tableName");
+//        Map<String,String> map = new HashMap<String,String>();
+//        map.put("tableName", tableName);
+//        DbFactory.Open("hbase").selectList("datamodeling.createHbaseTable",map);
+//        DbFactory.close("hbase");
+//        return "";
+//    }
+
+    @RequestMapping(value="/createHbaseTable",produces = "text/plain;charset=UTF-8")
+    public String createHbaseTable2(@RequestBody JSONObject pJson)  {
+        String tableName = pJson.getString("tableName");
+        String tableFields = pJson.getString("tableFields");
+        String primaryKey = pJson.getString("primaryKey");
+        JSONArray tableFieldsArray=JSONArray.parseArray(tableFields);
+
+
+        //构建表语句
+        StringBuffer tableSql=new StringBuffer();
+        tableSql.append("CREATE  TABLE IF NOT EXISTS  ");
         tableSql.append("  ");
         tableSql.append(tableName);
 
@@ -314,30 +358,23 @@ public class DataModeling extends RO
 
         }
         tableSql.deleteCharAt(tableSql.length()-1);
+        tableSql.append("   CONSTRAINT PK PRIMARY KEY  ");
+        tableSql.append("  (");
+        tableSql.append(primaryKey);
         tableSql.append("  )");
-
-        tableSql.append(" ROW FORMAT   ");
-        tableSql.append("  DELIMITED FIELDS TERMINATED BY ','");
-        tableSql.append("  STORED AS TEXTFILE ");
+        tableSql.append("  )");
 
         String aa =  tableSql.toString();
 
         Map<String,String> map = new HashMap<String,String>();
 
         map.put("tableSql", tableSql.toString());
-        DbFactory.Open("hive").selectList("datamodeling.createHiveTable2",map);
-        DbFactory.close("hive");
+        DbFactory.Open("hbase").selectList("datamodeling.createHbaseTable2",map);
+        DbFactory.close("hbase");
         return  tableSql.toString();
     }
 
 
-    @RequestMapping(value="/createHbaseTable",produces = "text/plain;charset=UTF-8")
-    public String createHbaseTable(@RequestBody JSONObject pJson)  {
-        String tableName = pJson.getString("tableName");
-        Map<String,String> map = new HashMap<String,String>();
-        map.put("tableName", tableName);
-        DbFactory.Open("hbase").selectList("datamodeling.createHiveTable",map);
-        DbFactory.close("hbase");
-        return "";
-    }
+
+
 }
