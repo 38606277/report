72c9ae6085170c3b77ab9d38a86f305e61ffdec5
gl36 <381219883@qq.com>
2021-01-19 11:29:13 +0800

    添加方法

---
+++ src/main/java/root/mqtt/controller/MqttTaskController.java
@@ -0,0 +1,78 @@
+package root.mqtt.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import root.mqtt.configure.MqttSendMessage;
+import root.mqtt.service.TopicService;
+import root.report.common.RO;
+import root.report.db.DbFactory;
+
+import javax.annotation.Resource;
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * 测试网关 1319100003
+ * @author cannon
+ */
+@RestController
+@RequestMapping(value = "/reportServer/mqttTask")
+public class MqttTaskController extends RO {
+	
+	@Resource
+	private TopicService topicService;
+	
+	@RequestMapping(value = "/getMqttTaskList", produces = "text/plain;charset=UTF-8")
+	public String getModelList(@RequestBody JSONObject pJson)  {
+		try {
+			Map<String,String> map=new HashMap();
+			map.put("startIndex",pJson.getString("startIndex"));
+			map.put("perPage",pJson.getString("perPage"));
+			map.put("model_name",pJson.get("model_name")==null?"":pJson.get("model_name").toString());
+			map.put("db_source",pJson.get("db_source")==null?"":pJson.get("db_source").toString());
+			map.put("db_type",pJson.get("db_type")==null?"":pJson.get("db_type").toString());
+			Map<String,Object> resultMap = topicService.getListPage(map);
+			return SuccessMsg("", resultMap);
+		} catch (Exception ex){
+			return ExceptionMsg(ex.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/getMqttTaskById", produces = "text/plain;charset=UTF-8")
+	public String getModelById(@RequestBody JSONObject pJson)  {
+		try{
+			Map resultObject =topicService.getMqttTaskByID(pJson);
+			return SuccessMsg("",resultObject);
+		}catch (Exception ex){
+			ex.printStackTrace();
+			return ExceptionMsg(ex.getMessage());
+		}
+
+	}
+
+	@RequestMapping(value = "/createMqttTask", produces = "text/plain;charset=UTF-8")
+	public String getTablesBySource(@RequestBody JSONObject pJson) throws SQLException {
+		SqlSession sqlSession =  DbFactory.Open(DbFactory.FORM);
+		try{
+			sqlSession.getConnection().setAutoCommit(false);
+			Map<String,Object> map  = this.topicService.saveOrUpdate(sqlSession,pJson);
+			if(Boolean.valueOf(map.get("status").toString())){
+				sqlSession.getConnection().commit();
+				return SuccessMsg("保存成功",map.get("info"));
+			}else{
+				return ErrorMsg("保存失败",map.get("info").toString());
+			}
+
+		}catch (Exception ex){
+			sqlSession.getConnection().rollback();
+			ex.printStackTrace();
+			return ExceptionMsg(ex.getMessage());
+		}
+	}
+}
--- src/main/java/root/mqtt/controller/TopicController.java
+++ src/main/java/root/mqtt/controller/TopicController.java
@@ -32,7 +32,7 @@ public class TopicController extends RO {
 	
 	@Resource
 	private TopicService topicService;
-	
+
 	@Resource
 	private MqttSendMessage mqttSendMessage;
 	
@@ -69,52 +69,4 @@ public class TopicController extends RO {
 //        return SuccessMsg("修改成功", "");
 //    }
 
-
-	@RequestMapping(value = "/getMqttTaskList", produces = "text/plain;charset=UTF-8")
-	public String getModelList(@RequestBody JSONObject pJson)  {
-		try {
-			Map<String,String> map=new HashMap();
-			map.put("startIndex",pJson.getString("startIndex"));
-			map.put("perPage",pJson.getString("perPage"));
-			map.put("model_name",pJson.get("model_name")==null?"":pJson.get("model_name").toString());
-			map.put("db_source",pJson.get("db_source")==null?"":pJson.get("db_source").toString());
-			map.put("db_type",pJson.get("db_type")==null?"":pJson.get("db_type").toString());
-			Map<String,Object> resultMap = topicService.getListPage(map);
-			return SuccessMsg("", resultMap);
-		} catch (Exception ex){
-			return ExceptionMsg(ex.getMessage());
-		}
-	}
-
-	@RequestMapping(value = "/getMqttTaskById", produces = "text/plain;charset=UTF-8")
-	public String getModelById(@RequestBody JSONObject pJson)  {
-		try{
-			Map resultObject =topicService.getMqttTaskByID(pJson);
-			return SuccessMsg("",resultObject);
-		}catch (Exception ex){
-			ex.printStackTrace();
-			return ExceptionMsg(ex.getMessage());
-		}
-
-	}
-
-	@RequestMapping(value = "/createMqttTask", produces = "text/plain;charset=UTF-8")
-	public String getTablesBySource(@RequestBody JSONObject pJson) throws SQLException {
-		SqlSession sqlSession =  DbFactory.Open(DbFactory.FORM);
-		try{
-			sqlSession.getConnection().setAutoCommit(false);
-			Map<String,Object> map  = this.topicService.saveOrUpdate(sqlSession,pJson);
-			if(Boolean.valueOf(map.get("status").toString())){
-				sqlSession.getConnection().commit();
-				return SuccessMsg("保存成功",map.get("info"));
-			}else{
-				return ErrorMsg("保存失败",map.get("info").toString());
-			}
-
-		}catch (Exception ex){
-			sqlSession.getConnection().rollback();
-			ex.printStackTrace();
-			return ExceptionMsg(ex.getMessage());
-		}
-	}
 }
